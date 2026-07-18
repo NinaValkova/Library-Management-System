8036ae524b3fd0593f5e9e49ad479df506f3a262
NinaValkova <nina.p.valkova@gmail.com>
2026-07-18 18:26:23 +0300

    Library management system

--- recommendation_service/main.py
+++ recommendation_service/main.py
@@ -6,12 +6,12 @@ app = FastAPI(
     title="Library Recommendation API",
     description="Spark-based recommender system",
     version="3.0"
-
+)
 
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:5173",  
+        "http://localhost:5173",
     ],
     allow_credentials=True,
     allow_methods=["*"],
